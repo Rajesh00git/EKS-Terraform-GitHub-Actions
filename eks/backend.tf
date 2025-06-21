198a36a8be2e851c80cf2557dc637ece5a1d4e78
Rajesh00git <rajeshsahu7978@gmail.com>
2025-06-22 02:41:33 +0530

    s3 bucket name added

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,16 +1,19 @@
 terraform {
   backend "s3" {
-    bucket         = "devsecops-database"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    bucket         = "devsecops-database"                          # S3 bucket name to store the state
+    key            = "EKS-TERRAFORM-GITHUB-ACTIONS/eks/terraform.tfstate" # Path/key in S3 bucket
+    region         = "us-east-1"                                   # AWS region
+    dynamodb_table = "Lock-Files"                                  # DynamoDB table for state locking
+    encrypt        = true                                          # Encrypt the state file at rest
   }
-  required_version = ">=0.13.0"
+
+  required_version = ">= 0.13.0"
+
   required_providers {
     aws = {
-      version = ">= 2.7.0"
       source  = "hashicorp/aws"
+      version = ">= 2.7.0"
     }
   }
 }
+
