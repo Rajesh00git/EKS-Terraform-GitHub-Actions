ec5dd4451e398f7bc8d8766d370d88b1d3fecba5
Rajesh00git <rajeshsahu7978@gmail.com>
2025-06-22 02:36:13 +0530

    s3 bucket name added

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,11 +1,4 @@
 terraform {
-  required_version = "~> 1.9.3"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49.0"
-    }
-  }
   backend "s3" {
     bucket         = "devsecops-database"
     region         = "us-east-1"
@@ -13,8 +6,11 @@ terraform {
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region  = var.aws-region
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
 }
